a4e84a571e31a3338b4a944067f93c33e0dec14a
Sajjad Alizadeh <39870083+Poker-Face-Vii@users.noreply.github.com>
2020-08-22 23:41:27 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# BoilerPlate Starter Kit
+
+A Flutter project useing Mobx and provider For states and Hive for database.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+## Boilerplate Features:
+
+* Sailers => For routeing
+* Mobx 
+* provider
+* Hive
+
+![anchor_image](https://raw.githubusercontent.com/gurleensethi/sailor/master/images/anchor-icon.png)
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:Flutter_boilerplate_kit/Api/Chopper_api.dart';
+import 'package:Flutter_boilerplate_kit/Router/sailor__Router.dart';
+import 'package:Flutter_boilerplate_kit/Stores/SampleMobx.dart';
+import 'package:Flutter_boilerplate_kit/Views/Home.Page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  Routes.createRoutes();
+  runApp(MyApp());
+}
+
+// this is my initial COMMIted So Be carfull of course !== => fklsdj
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [   Provider(
+          create: (context) => ApiService.create(),
+          dispose: (_, ApiService service) => service.client.dispose(),
+        ),
+        Provider(
+          create: (_) => SampleStore(), // this is sample Mobx Store.
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'BoilerPlate Kit',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: Routes.sailor.generator(),
+        navigatorKey: Routes.sailor.navigatorKey,
+        home: HomePage(),
+      ),
+    );
+  }
+}
